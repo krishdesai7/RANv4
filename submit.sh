72643e2bc8212fcc83bbbf2b996d12a35dc65967
krishdesai7 <krishdesai7@gmail.com>
2026-03-15 17:34:55 -0700

    Update submit.sh to extend SLURM job time limit from 1 hour to 2 hours for improved resource allocation during job submissions.

--- submit.sh
+++ submit.sh
@@ -3,7 +3,7 @@
 #SBATCH --constraint=gpu
 #SBATCH --gpus=4
 #SBATCH --account=m3246_g
-#SBATCH --time=01:00:00
+#SBATCH --time=02:00:00
 #SBATCH --output=slurm-%j.log
 
 cd /global/u1/k/kdesai/RANv4
